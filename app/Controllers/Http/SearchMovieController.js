26e357d0e1761e97ae4c28d87bc94d0f08100247
unknown <albertopopescu@outlook.com>
2021-11-17 00:15:51 +0000

    working up to min 21:20

--- app/Controllers/Http/SearchMovieController.js
+++ app/Controllers/Http/SearchMovieController.js
@@ -3,12 +3,21 @@
 const { validate } = use('Validator');
 
 class SearchMovieController {
-    async index({ request, view }) { 
+    async index({ request, view, session, response }) { 
         const rules = {
             search: 'required'
         }
 
         const validation = await validate(request.all(), rules);
+
+        if (validation.fails()) { 
+            session.withErrors(validation.messages()).flashAll();
+            return response.redirect('back');
+        }
+
+
+        // search on ombd api
+
         return view.render('pages/search-result');
     }
 }
